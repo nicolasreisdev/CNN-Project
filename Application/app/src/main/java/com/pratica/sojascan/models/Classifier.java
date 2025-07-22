b4841f686fced95f2c08da5ea79e943cfaf0882e
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-07-22 20:18:50 -0300

    Organizando e iniciando aplicativo que irá utilizar o modelo CNN.

--- Application/app/src/main/java/com/pratica/sojascan/models/Classifier.java
+++ Application/app/src/main/java/com/pratica/sojascan/models/Classifier.java
@@ -1,4 +1,20 @@
 package com.pratica.sojascan.models;
 
+
+// Regra de negócio - classe responsável por toda a interação com o modelo PyTorch.
 public class Classifier {
+    public Classifier() {
+    }
+
+    public void loadModel(){
+
+    }
+
+    public void preprocess(){
+
+    }
+
+    public void predict(){
+
+    }
 }
