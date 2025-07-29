6b00fdd3b9b9c64dffd439262599cf0c4c236dec
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-07-29 17:32:16 -0300

    Adicionando conversor (teste).

---
+++ Conversor/converter.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision.models as models
+from torch.utils.mobile_optimizer import optimize_for_mobile
+
+NUM_CLASSES = 11 
+
+input_pkl_file = './ASDID/SqueezeNet-ASDID-91.70.pkl'
+output_ptl_file = './modeloMobile/squeezenet_mobile.ptl'
+model_architecture = models.squeezenet1_0(num_classes=NUM_CLASSES)
+
+try:
+    if torch.cuda.is_available():
+        my_device = torch.device("cuda:0")
+    else:
+        my_device = torch.device("cpu")
+
+    model_architecture.load_state_dict(torch.load(input_pkl_file, map_location=my_device)) # carregar o modelo treinado
+    
+    model_architecture.eval() # definir o modelo para o modo de avaliação
+
+    scripted_model = torch.jit.script(model_architecture) # converter o modelo para o formato TorchScript
+
+    optimized_model = optimize_for_mobile(scripted_model) # otimizar o modelo para dispositivos móveis
+
+    optimized_model._save_for_lite_interpreter(output_ptl_file) # salvar o modelo otimizado
+
+except Exception as e:
+    print(f"\nOcorreu um erro durante a conversão: {e}")
